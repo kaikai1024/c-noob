50e00bfb3d80db5af42baa07d301faeaef11f54c
kaikai1024 <281959824@qq.com>
2015-08-26 11:31:24 +0800

    Update analy_pkt.c

--- c2/analy_pkt.c
+++ c2/analy_pkt.c
@@ -1,8 +1,34 @@
-
+/*
+*
+*\file                  analy_pkt.c
+*\brief                 源文件
+*\detail				分析数据包
+*
+*\author				冯开开
+*\version				v1
+*\date					2015.8.25
+*
+*/
 #include "analy_pro.h"
 #include "mailAnalysis.h"
+#include "debug.h"
 
+#define SUCCESS (0)
 
+/*
+ *\fn					int analy_pkt(char buf[], unsigned int len);
+ *\brief				分析包
+ *\details				通过指针到移动依次分析包到结构
+ *
+ *
+ *\param(in)			buf字符数组，数据包到长度
+ *\param(out)			
+ *
+ *\return				void
+ *\retval
+ *
+ * \note
+ * */
 int analy_pkt(char buf[], unsigned int len)
 {
 
@@ -12,89 +38,91 @@ int analy_pkt(char buf[], unsigned int len)
 	struct tcphdr *p_tcp;
 	struct udphdr *p_udp;
 	struct icmphdr *p_icmp;
-	char * tmp = NULL;
-	unsigned short eth_type;
+
+	char * tmp 				= NULL;
+	unsigned short eth_type = 0;
+
 	list * from = initList();
-	list * to = initList();
+	list * to   = initList();
 
 	if(len < sizeof(struct ether_header))
 	{
-		debug(0, "size < ethhdr");
-		return 0;
+		IPIKE_CFG_INFO_INFO("size < ethhdr");
+		return SUCCESS;
 	}
-	p_eth = (struct ether_header *)buf;
-	analy_ether(p_eth);
+
+	p_eth	 = (struct ether_header *)buf;
 	eth_type = ntohs(p_eth->ether_type);
+	//analy_ether(p_eth);
 	
-	if(ETHERTYPE_IP == eth_type )
+	if (ETHERTYPE_IP == eth_type)
 	{
-		if(len < sizeof(struct ether_header) + sizeof(struct ip))
+		if (len < sizeof(struct ether_header) + sizeof(struct ip))
 		{
-			debug(0, "size < ethhdr + iphdr");
-			return 0;
+			IPIKE_CFG_INFO_INFO("size < ethhdr + iphdr");
+			return SUCCESS;
 		}
+
 		p_ip = (struct ip *)(buf + sizeof (struct ether_header));
-		analy_ip(p_ip);
+		//analy_ip(p_ip);
 
-		switch(p_ip->ip_p)
+		switch ( p_ip->ip_p )
 		{
-			case IPPROTO_TCP:
-					if(len < sizeof(struct ether_header)+sizeof(struct ip)+sizeof(struct tcphdr))
-					{
-						debug(0, "size < ethhdr + iphdr + tcphdr");
-						return 0;
-					}
-					p_tcp = (struct tcphdr *)(buf + sizeof(struct ether_header) + sizeof(struct ip));
-					analy_tcp(p_tcp);
+		case IPPROTO_TCP:
+			if(len < sizeof(struct ether_header) + sizeof(struct ip)+sizeof(struct tcphdr))
+		    {
+		    	IPIKE_CFG_INFO_INFO("size < ethhdr + iphdr + tcphdr");
+				return SUCCESS;
+			}
+			p_tcp = (struct tcphdr *)(buf + sizeof(struct ether_header) + sizeof(struct ip));
+			analy_tcp(p_tcp);
 
-					if(587 == p_tcp->source || 587 == p_tcp->dest)
-					{
-						debug(0, "\n\n\nsmtp\n\n\n");
-						tmp = buf + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr);
-						mailAnalysis(tmp,len,from,to);
-					}
+			if(587 == ntohs( p_tcp->source) || 587 == ntohs(p_tcp->dest) )
+			{
+				IPIKE_CFG_INFO_INFO("\n\n\nsmtp\n\n\n");
+				tmp = buf + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr);
+				mailAnalysis(tmp, len, from, to);
+			}
 
-					if(110 == p_tcp->source || 110 == p_tcp->dest)
-					{
-						debug(0, "\n\n\npop\n\n\n");
-						//tmp = buf + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr);
-						//mailAnalysis(tmp,len);
-					}
+			if(110 == ntohs(p_tcp->source) || 110 == ntohs(p_tcp->dest) )
+			{
+				IPIKE_CFG_INFO_INFO("\n\n\npop\n\n\n");
+			}
+			break;
 
-					break;
+		case IPPROTO_UDP:
+			if(len < sizeof(struct ether_header)+sizeof(struct ip) + sizeof(struct udphdr))
+			{
+				IPIKE_CFG_INFO_INFO("size < ethhdr + iphdr + udphdr");
+				return SUCCESS;
+			}
+			p_udp = (struct udphdr *)(buf + sizeof(struct ether_header) +  sizeof(struct ip));
+			//analy_udp(p_udp);
+			break;
 
-				case IPPROTO_UDP:
-					if(len < sizeof(struct ether_header)+sizeof(struct ip)+sizeof(struct udphdr))
-					{
-						debug(0, "size < ethhdr + iphdr + udphdr");
-						return 0;
-					}
-					p_udp = (struct udphdr *)(buf + sizeof(struct ether_header) +  sizeof(struct ip));
-					analy_udp(p_udp);
-					break;
+		case IPPROTO_ICMP:
+			if(len < sizeof(struct ether_header)+sizeof(struct ip)+sizeof(struct icmphdr))
+			{
+				IPIKE_CFG_INFO_INFO("size < ethhdr + iphdr + icmphdr");
+				return SUCCESS;
+			}
 
-				case IPPROTO_ICMP:
-					if(len < sizeof(struct ether_header)+sizeof(struct ip)+sizeof(struct icmphdr))
-					{
-						debug(0, "size < ethhdr + iphdr + icmphdr");
-						return 0;
-					}
-					p_icmp = (struct icmphdr *)(buf + sizeof(struct ether_header) + sizeof(struct ip));
-					analy_icmp(p_icmp);
-					break;
-				default:
-					debug(p_ip->ip_p, "ip--other");
-					return 0;
-					break;
+			p_icmp = (struct icmphdr *)(buf + sizeof(struct ether_header) + sizeof(struct ip));
+			analy_icmp(p_icmp);
+			break;
+		default:
+			IPIKE_CFG_INFO_INFO("ip--other");
+			return SUCCESS;
 		}
+
 	}
 
 	else if(ETHERTYPE_ARP == eth_type)
 	{
 		if(len < sizeof(struct ether_header) + sizeof(struct arphdr))
 		{
-			debug(0, "size < ethhdr + arphdr");
-			return 0;
+			IPIKE_CFG_INFO_INFO("size < ethhdr + arphdr");
+			return SUCCESS;
 		}
 
 		p_arp = (struct arphdr *)(buf + sizeof (struct ether_header));
@@ -105,19 +133,20 @@ int analy_pkt(char buf[], unsigned int len)
 	{
 		if(len < sizeof(struct ether_header) + sizeof(struct arphdr))
 		{
-			debug(0, "size < ethhdr + arphdr");
-			return 0;
+			IPIKE_CFG_INFO_INFO("size < ethhdr + arphdr");
+			return SUCCESS;
 		}
+
 		p_arp = (struct arphdr *)(buf + sizeof (struct ether_header));
 		analy_rarp(p_arp);
 	}
 
 	else
 	{
-		debug(eth_type, "ether--other");
-		return 0;
+		IPIKE_CFG_INFO_INFO("ether--other");
+		return SUCCESS;
 	}
 
-	return 0;	
+	return SUCCESS;	
 }
 
