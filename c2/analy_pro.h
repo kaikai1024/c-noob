8c49569326a999836a7de94c4d6672f7c1a2af99
kaikai1024 <281959824@qq.com>
2015-08-26 11:32:21 +0800

    Update analy_pro.h

--- c2/analy_pro.h
+++ c2/analy_pro.h
@@ -1,8 +1,8 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+/*各层协议头定义*/
 #include <netinet/in.h>  
 #include <netinet/ip.h>  
 #include <netinet/tcp.h>  
@@ -13,10 +13,17 @@
 #include <arpa/inet.h>   
 #include <sys/types.h>
 
+/*分析链路层协议*/
 void analy_ether(struct ether_header * p_eth);
+/*分析ip协议*/
 void analy_ip(struct ip * p_ip);
+/*分析arp协议*/
 void analy_arp(struct arphdr * p_arp);
+/*分析rarp协议*/
 void analy_rarp(struct arphdr * p_arp);
+/*分析tcp协议*/
 char * analy_tcp(struct tcphdr * p_tcp);
+/*分析udp协议*/
 void analy_udp(struct  udphdr * p_udp);
+/*分析icmp协议*/
 void analy_icmp(struct icmphdr * p_icmp);
